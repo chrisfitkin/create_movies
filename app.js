27a40a89546dccdcf05acf24b9646ea078932a01
chrisfitkin <cfitkin@gmail.com>
2016-10-16 18:35:01 -0700

    adding response page

--- app.js
+++ app.js
@@ -20,15 +20,54 @@ app.get('/', function(req, res, next) {
     res.render('createMovie');
 });
 
-app.post('/favorite_fruit', function(req, res, next) {
-    var favorite = req.body.fruit;
-    if (typeof favorite == 'undefined') {
-        next('Please choose a fruit!');
-    }
-    else {
-        res.send("Your favorite fruit is " + favorite);
-    }
+app.post('/create_movie', function(req, res, next) {
+  var title = req.body.title;
+  var year = req.body.year;
+  var imdb = req.body.imdb;
+  if (typeof title == 'undefined') next('Please provide a Title');
+  if (typeof year == 'undefined') next('Please provide a Year');
+  if (typeof imdb == 'undefined') next('Please provide an IMDB ID');
+
+  var newMovie = {
+    title: title,
+    year: year,
+    imdb: imdb
+  }
+
+  var url = 'mongodb://localhost:27017/video';
+
+  MongoClient.connect(url, function(err, db) {
+
+      assert.equal(null, err);
+      console.log("Successfully connected to server");
+
+      // Add the movie to the database
+      db.collection('movies').insertOne(newMovie).toArray(function(err, doc) {
+
+          // Print
+          if(doc.acknowledged) {
+            res.send(newMovie.Title + " added using insertOne() with _id = " + doc.insertedId);
+          } else {
+            next("Could not insert movie :(");
+          }
+
+          // More logging
+          console.log("Movie added with insertOne");
+
+          db.close();
+      });
+  });
 });
+
+// app.post('/favorite_fruit', function(req, res, next) {
+//     var favorite = req.body.fruit;
+//     if (typeof favorite == 'undefined') {
+//         next('Please choose a fruit!');
+//     }
+//     else {
+//         res.send("Your favorite fruit is " + favorite);
+//     }
+// });
 
 app.use(errorHandler);
 
