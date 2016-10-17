6d753d94c3b247e266deec3c0305086cbb8cfa84
chrisfitkin <cfitkin@gmail.com>
2016-10-16 19:39:33 -0700

    fixed header error

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ function errorHandler(err, req, res, next) {
 }
 
 app.get('/', function(req, res, next) {
-    // res.render('fruitPicker', { 'fruits' : [ 'apple', 'orange', 'banana', 'peach' ] });
     res.render('createMovie');
 });
 
@@ -32,50 +31,25 @@ app.post('/create_movie', function(req, res, next) {
       var year = req.body.year;
       var imdb = req.body.imdb;
       if (typeof title == 'undefined') next('Please provide a Title');
-      if (typeof year == 'undefined') next('Please provide a Year');
-      if (typeof imdb == 'undefined') next('Please provide an IMDB ID');
+      else if (typeof year == 'undefined') next('Please provide a Year');
+      else if (typeof imdb == 'undefined') next('Please provide an IMDB ID');
+      else {
+        assert.equal(null, err);
 
-      var newMovie = {
-        'title': title,
-        'year': year,
-        'imdb': imdb
-      };
-
-
-      assert.equal(null, err);
-      // console.log("Successfully connected to server");
-
-      // Add the movie to the database
-      // try {
-        // db.collection('movies').insertOne( newMovie );
         db.collection('movies').insertOne(
             { 'title': title, 'year': year, 'imdb': imdb },
             function (err, r) {
                 assert.equal(null, err);
-                res.send("Document inserted with _id: " + r.insertedId);
+                res.send("Movie inserted with _id: " + r.insertedId);
             }
         );
-      // } catch (e) {
-      //   next(Error("Could not insert movie :("));
-      // }
 
-      // console.log("Movie added with insertOne");
-      res.end(newMovie.title + " added with insertOne()");
-
+      }
       db.close();
 
   });
 });
 
-// app.post('/favorite_fruit', function(req, res, next) {
-//     var favorite = req.body.fruit;
-//     if (typeof favorite == 'undefined') {
-//         next('Please choose a fruit!');
-//     }
-//     else {
-//         res.send("Your favorite fruit is " + favorite);
-//     }
-// });
 
 app.use(errorHandler);
 
